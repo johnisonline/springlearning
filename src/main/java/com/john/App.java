1f322fe0879858b70e475c5f602b7a251b8d523d
johnisonline <johnisonline@gmail.com>
2014-12-23 15:29:03 -0500

    sample project

---
+++ src/main/java/com/john/App.java
@@ -0,0 +1,15 @@
+package com.john;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Person person = new Person();
+        person.speak();
+      //  System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/john/Person.java
@@ -0,0 +1,9 @@
+package com.john;
+
+public class Person{
+
+    public Person(){}
+
+    public void speak()
+    {System.out.println("im a person");}
+}
